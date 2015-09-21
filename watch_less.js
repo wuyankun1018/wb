961f2bb8c159f1e6d98b9565257a1bd0727cb955
吴焰坤 <wuyankun@wuyankundeiMac.local>
2015-09-21 12:32:08 +0800

    pc register

--- watch_less.js
+++ watch_less.js
@@ -1,9 +1,9 @@
 var fs = require('fs')
 	,exec = require('child_process').exec
-fs.watch('/Users/wuyankun/Desktop/wdqc/less', { persistent: true, interval: 500 }, function(e, filename){
+fs.watch('./wdpc/less', { persistent: true, interval: 500 }, function(e, filename){
 	console.log(filename,e)
 	var file = filename.slice(0,-5)
-	var s = 'lessc /Users/wuyankun/Desktop/wdqc/less/'+filename+' /Users/wuyankun/Desktop/wdqc/css/'+file+'.css'
+	var s = 'lessc ./wdpc/less/'+filename+' ./wdpc/css/'+file+'.css'
 	exec(s, function(){
 		// console.log(arguments)
 	})
