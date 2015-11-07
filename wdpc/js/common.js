967295d5baf7fb43bad76640e5c06b04f2e43a4b
yankunwu <yankunwu@meilishuo.com>
2015-11-07 14:54:27 +0800

    fix css

--- wdpc/js/common.js
+++ wdpc/js/common.js
@@ -1,16 +1,21 @@
 $(function(){
 	var scrollTop = 0;
-	$(document,window,'body').on('scroll', function(event) {
-		var _scrollTop = $(this).scrollTop()
-		if(_scrollTop> scrollTop){
-			$('.footer_container').find('.for_fix_wrap').addClass('fixed')
-		} else {
-			$('.footer_container').find('.for_fix_wrap').removeClass('fixed')
-		}
-		scrollTop = _scrollTop
+	var timeoutFlag = 0;
+	$(window).on('scroll', function(event) {
+		if(timeoutFlag) clearTimeout(timeoutFlag);
+		timeoutFlag = setTimeout(function(){
+			var _scrollTop = $(this).scrollTop()
+			if(_scrollTop< scrollTop){
+				$('.footer_container').find('.for_fix_wrap').addClass('fixed')
+			} else {
+				$('.footer_container').find('.for_fix_wrap').removeClass('fixed')
+			}
+			scrollTop = _scrollTop
+			timeoutFlag = 0
+		},100)
 	});
 	$('.gotop').on('click', function(event) {
 		event.preventDefault();
-		$('body').scrollTop(0)
+		$(window).scrollTop(0)
 	});
 })
