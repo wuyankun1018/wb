b9209684332ed1e2c00db89d41c348763e83c381
吴焰坤 <wuyankun@wuyankundeiMac.local>
2015-12-04 10:51:45 +0800

    auto change

--- wdpc/js/gaizhuangdian.js
+++ wdpc/js/gaizhuangdian.js
@@ -20,4 +20,28 @@ $(function(){
 			marginLeft: -index*width
 		});
 	}
+
+	$('.gzd_header').on('click', '.item', function(event) {
+		$(this).addClass('act').siblings().removeClass('act')
+		$('.main_img').find('img').attr('src', $(this).attr('src'))
+		$('.main_img').find('.img_tle_wrap').text($(this).data('title'))
+	});
+
+	var bannerAutoFlag = false; //是否自动轮播
+	$('.tuku_header').on('mouseleave', function(event) {
+		bannerAutoFlag = false
+	}).on('mouseenter', function(event) {
+		bannerAutoFlag = true
+	})
+
+	window.setInterval(function(){
+		if(bannerAutoFlag) return;
+		var $tleItems = $('.thumb_wrapper').find('.item')
+		var nextIndex = $tleItems.filter('.act').index()
+		nextIndex++
+		if(nextIndex<0 || nextIndex>$tleItems.length-1){
+			nextIndex = 0;
+		}
+		$tleItems.eq(nextIndex).trigger('click')
+	}, 5000)
 })
