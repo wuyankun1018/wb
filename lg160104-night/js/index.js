bd5d36b27d056dbe38e7357117d68375097be4bd
yankunwu <yankunwu@meilishuo.com>
2016-01-04 18:01:47 +0800

    night

--- lg160104-night/js/index.js
+++ lg160104-night/js/index.js
@@ -14,3 +14,106 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
           };
 })();
+
+var pageViewController = new function(){
+	var $parent = $('.page_container')
+	var currIndex = 0
+	var pageLength = $parent.children('.page_item').length
+
+	this.next = function(){
+		currIndex++
+		if(currIndex>pageLength-1){
+			currIndex--;
+			return;
+		}
+		reloadView((currIndex-1)*100)
+	}
+
+	this.prev = function(){
+		currIndex--
+		if(currIndex<0){
+			currIndex=0
+			return;
+		}
+		reloadView((currIndex+1)*100)
+	}
+
+	var isMove = false
+
+	function reloadView(start){
+		// var percentVal = -currIndex*100 + '%'
+		// var tranlateY = 'translateY('+percentVal+')'
+		// $parent.css({
+		// 	'transform':tranlateY
+		// 	,'-webkit-transform':tranlateY
+		// })
+		if(isMove) return;
+		isMove = true;
+		$('#turn_page_tip').hide()
+		moveUp($parent, start, currIndex*100, 0.6, 0).done(function(){
+			$('#turn_page_tip').show()
+			isMove = false;
+		})
+	}
+
+	function moveUp(ele, sY, tY, sTime, delay){
+		var _this = this
+		sY = sY ||0
+	 	var dtd = $.Deferred();
+	 	var perT = 1000/60
+	 	var msTime = sTime*1000
+	 	var frames = (msTime/perT)|0
+	 	delay = delay||0
+	 	var doneFrameCount = 0
+	 	var change = tY - sY
+	 	function _move(){
+	 		if(doneFrameCount<frames){
+	 			var len = easeInOut(doneFrameCount,sY,change,frames)
+	 			changeTranslateY(len)
+	 			window.requestAnimFrame(_move)
+	 		} else {
+	 			changeTranslateY(tY)
+	 			dtd.resolve(_this)
+	 		}
+	 		doneFrameCount++
+	 	}
+	 	function easeInOut(t, b, c, d) {
+	 		//t => time(初始记步次数)   b => begin(初始位置)   c => change(变化量)   d => duration(持续次数)
+	        if ((t /= d / 2) < 1) return c / 2 * t * t + b;
+	        return - c / 2 * ((--t) * (t - 2) - 1) + b;
+	    }
+	    setTimeout(_move, delay*1000)
+	    // fortest
+	    // doneFrameCount = frames
+	    // _move()
+	    function changeTranslateY(percentVal){
+	    	var percentVal = -percentVal+ '%'
+			var tranlateY = 'translateY('+percentVal+')'
+	    	ele.css({
+				'transform':tranlateY
+				,'-webkit-transform':tranlateY
+			})
+	    }
+	    return dtd.promise();
+	}
+}
+//禁止默认滑动事件
+document.addEventListener('touchmove', function (event) {
+	event.preventDefault();
+}, false);
+$('.main_container').on('swipeUp', function(event) {
+	event.preventDefault();
+	pageViewController.next()
+}).on('swipeDown', function(event) {
+	event.preventDefault();
+	pageViewController.prev()
+});
+
+$('.share.btn').on('tap', function(event) {
+	$('#share_masker').show()
+});
+
+$('#share_masker').on('tap', function(event) {
+	event.preventDefault();
+	$(this).hide()
+});
